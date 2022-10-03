ebc1122f60ca71362734b088110c8c9535081f04
IHEANYI, FAVOUR CHISOM <iheanyifavour.ube3room312@gmail.com>
2022-10-03 22:44:42 +0100

    added somethings to my-flask-project

--- website/__init__.py
+++ website/__init__.py
@@ -24,7 +24,11 @@ def create_app():
 
     login_manager = LoginManager()
     login_manager.login_view = "auth.login"
-    
+    login_manager.init_app(app)
+
+    @login_manager.user_loader
+    def load_user(id):
+        return User.query.get(init(id))
 
     return app
 
