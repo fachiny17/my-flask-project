8fe6b6b53bd791bb129ac4ecf99297bc0e8048c6
IHEANYI, FAVOUR CHISOM <iheanyifavour.ube3room312@gmail.com>
2022-10-06 14:21:36 +0100

    Updated my-flask-project

--- website/__init__.py
+++ website/__init__.py
@@ -18,7 +18,7 @@ def create_app():
     app.register_blueprint(views, url_prefix="/")
     app.register_blueprint(auth, url_prefix="/")
 
-    from .model import User
+    from .models import User
 
     create_database(app)
 
